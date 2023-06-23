76fc9df0398618a13bc76ae93ed71477312ad490
arhanz <96288469+arhanz@users.noreply.github.com>
2023-06-23 08:53:08 +0500

    add files

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script src="https://cdn.tailwindcss.com"></script>
+      <script src="https://kit.fontawesome.com/e79e73d12c.js" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<link href="https://unpkg.com/swiper/swiper-bundle.min.css" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
